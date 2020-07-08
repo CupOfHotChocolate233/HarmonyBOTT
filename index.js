19a9b3dc4c138e4d0cc334d8e60c35945435b783
CupOfHotChocolate233 <67971039+CupOfHotChocolate233@users.noreply.github.com>
2020-07-07 21:27:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const bot = new Discord.Client();
 
 bot.on('ready', ()=>{
     console.log('system online.....')
-    bot.user.setActivity('Vibing right now... Levae me alone >:C', { type: 'STREAMING'}).catch(console.error)
+    bot.user.setActivity('Vibing right now... Levae me alone >:C', { type: 'LISTENING'}).catch(console.error)
     
- 
+
 });
 
 bot.login(process.env.token);
