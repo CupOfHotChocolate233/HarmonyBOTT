624dbe6adc05745c3889e00e58e3952bf7755a99
CupOfHotChocolate233 <67971039+CupOfHotChocolate233@users.noreply.github.com>
2020-07-07 21:19:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const bot = new Discord.Client();
 
 bot.on('ready', ()=>{
     console.log('system online.....')
-
+    bot.user.setActivity('Vibing right now... Levae me alone >:C')
     bot.user.setStatus('idle')
-    bot.user.setGame('yes')
+ 
 });
 
 bot.login(process.env.token);
